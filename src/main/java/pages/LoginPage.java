e722af1e5a2fac65a95336624bb17faf5a8e9ac4
RK <RK@172.16.2.168>
2018-01-08 11:29:09 +0530

    loginPage

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,48 @@
+package pages;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.relevantcodes.extentreports.ExtentTest;
+
+import wrappers.LeafTapsWrappers;
+
+public class LoginPage extends LeafTapsWrappers{
+	
+	public LoginPage (RemoteWebDriver driver, ExtentTest test){
+		this.driver  =driver;
+		this.test  =test;
+		if(verifyTitle("Opentaps Open Source ERP + CRM")==false){			
+			reportStep("This is not My Login Page", "Fail");
+		}
+		
+	}
+	public LoginPage enterUserName(String username){
+		enterById("username",data);
+		return this;
+	}
+	
+	public LoginPage enterPassword(String data){
+		enterById("password", data);
+		return this;
+	}
+	public HomePage clickLogin(){
+		clickByClassName("decorativeSubmit");
+		return new HomePage(driver,test);
+	}
+	public LoginPage failedLogin(){
+		clickByClassName("decorativeSubmit");
+		return this;
+	}
+	public void verifyErrorMsg(String data) {
+		verifyTextContainsById("errorDiv", data);
+	}
+	public LoginPage enterConfirmPassword(String data) {
+		enterById("Confirmpassword", data);
+		return null;
+	}
+	public LoginPage enterEmailId(String emailId) {
+		enterByXpath("EmailId",data);
+		return null;
+	}
+	
+}
