a075154ada661b916f2d3cc3f10e066ab4748b8a
tahir <tahir@172.16.2.109>
2018-01-09 11:46:08 +0530

    I edited some methods in AppFile Create Lead

---
+++ CreateLead/src/main/java/CreateLead/CreateLead/App.java
@@ -0,0 +1,38 @@
+package CreateLead.CreateLead;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static void main1(String st)
+    {
+        System.out.println( "Hello World!" );
+    }
+    public static void main1(int i)
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static void main1(int i,int j)
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static void main1(int i,int j,int k)
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    
+    public static void main1(char ch,char ch1)
+    {
+        System.out.println( "Hello World!" );
+    }
+}
